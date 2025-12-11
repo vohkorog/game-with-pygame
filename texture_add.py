80ada9db4e9dcdd01e7868b0e0d186449fe1c9f1
Горохов Кирилл <GKA@planeta.stend>
2025-12-11 16:03:29 +0300

    добавление разного стиля рисования текстуры (удержанием ЛКМ и отдельными кликами)

--- texture_add.py
+++ texture_add.py
@@ -10,6 +10,8 @@ class TextureAdd:
         self.position = [0, 0] 
         self.mouse_click = 0
         self.list_entity = []
+        self.last_mouse_state = False
+        self.paint_style_state = 0
 
     def update(self):
         self.input()
@@ -18,16 +20,36 @@ class TextureAdd:
             self.list_entity.append(entity)
         
     def input(self):
-        event = pygame.mouse.get_pressed()
+        event_mouse = pygame.mouse.get_pressed()
+        event_keyboar = pygame.key.get_pressed()
 
-        if event[0]:
-            self.mouse_click = 1
-            self.position = pygame.mouse.get_pos()
-            print(self.position)    
-        else:
-            self.mouse_click = 0
-        if event[2]:
-            self.list_entity.clear()
+        if event_keyboar[pygame.K_1]:
+            self.paint_style_state = 0
+        if event_keyboar[pygame.K_2]:
+            self.paint_style_state = 1
+        
+        if self.paint_style_state == 0:
+            if event_mouse[0] and not self.last_mouse_state:
+                self.mouse_click = 1
+                self.position = pygame.mouse.get_pos()
+                print(self.position)    
+            else:
+                self.mouse_click = 0
+            if event_mouse[2]:
+                self.list_entity.clear()
+            
+            self.last_mouse_state = event_mouse[0]
+
+        if self.paint_style_state == 1:
+            if event_mouse[0]:
+                self.mouse_click = 1
+                self.position = pygame.mouse.get_pos()
+                print(self.position)    
+            else:
+                self.mouse_click = 0
+            if event_mouse[2]:
+                self.list_entity.clear()
+            
 
     def render(self, window):
         for entit in self.list_entity:
